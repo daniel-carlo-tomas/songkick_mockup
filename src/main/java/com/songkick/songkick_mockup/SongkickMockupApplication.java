06a61ed5211503e1000e0b21363c086bb300f91a
danielstrolle <danielstrolle@gmail.com>
2017-11-17 10:08:08 -0600

    Initial commit

---
+++ src/main/java/com/songkick/songkick_mockup/SongkickMockupApplication.java
@@ -0,0 +1,12 @@
+package com.songkick.songkick_mockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongkickMockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongkickMockupApplication.class, args);
+	}
+}
