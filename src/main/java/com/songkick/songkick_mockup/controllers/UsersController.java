c0df34c21173e48a412bc3ec093aaf1c69a9f849
Bolimbo <carlo.cappellini@yahoo.com>
2017-11-17 16:58:53 -0600

    created users controllers and hard coded first user into database

--- src/main/java/com/songkick/songkick_mockup/controllers/UsersController.java
+++ src/main/java/com/songkick/songkick_mockup/controllers/UsersController.java
@@ -1,16 +1,51 @@
 package com.songkick.songkick_mockup.controllers;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.songkick.songkick_mockup.models.User;
+import jdk.nashorn.internal.runtime.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import com.songkick.songkick_mockup.repositories.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UsersController {
 
     private UsersRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+
 
     @Autowired
+    public UsersController( UsersRepository userRepository){
+        this.userRepository = userRepository;
 
+    }
+
+
+    @GetMapping("/register")
+    public String ShowRegisterForm(Model model){
+        User user = new User();
+        user.setUsername("carlo");
+        user.setEmail("carlo@codeup.com");
+        user.setPassword("$$$");
+        user.setCity("San Antonio");
+        user.setState("Texas");
+        userRepository.save(user);
+        return "/users/register";
+    }
+
+//    @PostMapping("/register")
+//    public String saveUser( User user) {
+//    userRepository.save(user);
+//
+//        return "redirect/login";
+//
+//
+//
+//
+//    }
 }
