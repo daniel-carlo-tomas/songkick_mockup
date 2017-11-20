18e53910ea26b1f2001aab4de90b11dd5bbfa827
Bolimbo <carlo.cappellini@yahoo.com>
2017-11-20 09:24:44 -0600

    configuration method added

--- src/main/java/com/songkick/songkick_mockup/SongkickMockupApplication.java
+++ src/main/java/com/songkick/songkick_mockup/SongkickMockupApplication.java
@@ -2,11 +2,17 @@ package com.songkick.songkick_mockup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SongkickMockupApplication {
-
+public class SongkickMockupApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SongkickMockupApplication.class, args);
+	}
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SongkickMockupApplication.class);
 	}
 }
