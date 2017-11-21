16a71d717d9bc56ea3c2685c151802067af0975f
Bolimbo <carlo.cappellini@yahoo.com>
2017-11-21 15:18:22 -0600

    fixed changes

--- src/main/java/com/songkick/songkick_mockup/repositories/FriendsRepository.java
+++ src/main/java/com/songkick/songkick_mockup/repositories/FriendsRepository.java
@@ -2,7 +2,8 @@
 
 package com.songkick.songkick_mockup.repositories;
 
-import com.songkick.songkick_mockup.models.FriendRequests;
+import com.songkick.songkick_mockup.models.FriendRequest;
+import com.songkick.songkick_mockup.models.FriendRequest;
 import com.songkick.songkick_mockup.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-    public interface FriendsRepository extends CrudRepository<FriendRequests, Long> {
+    public interface FriendsRepository extends CrudRepository<FriendRequest, Long> {
         //FriendRequests findOne(Long id);
-        List<FriendRequests> findByReceiver(User receiver);
+        List<FriendRequest> findByReceiver(User receiver);
     }
 
