c0278c3c7e310e1518b16060867aff44369afeb2
danielstrolle <danielstrolle@gmail.com>
2017-11-30 11:49:57 -0600

    Change the pathing of the home controller

--- src/main/java/com/songkick/songkick_mockup/controllers/HomeController.java
+++ src/main/java/com/songkick/songkick_mockup/controllers/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
     @GetMapping("/")
     public String home () {
-        return "/home";
+        return "home";
     }
 }
